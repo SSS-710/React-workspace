9f4ed3bc1566ab7a2f6e84ca15fe0ed0e5837d7a
Shashank Singh Sikarwar <shashanksss710@gmail.com>
2026-04-19 23:38:04 +0530

    Use State

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+
+  let a = 20;
+  function changeA(){
+    console.log(a);
+    a = 30
+    console.log(a);
+  }
+  return (
+    <div>
+      <h1>Value of a is {a}</h1>
+      <button onClick={changeA}>Click</button>
+    </div>
+  )
+}
+
+export default App
